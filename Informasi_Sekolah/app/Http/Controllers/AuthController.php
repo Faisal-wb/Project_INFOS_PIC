3fb94edde7c35eefe39dadfdebb51a8999223f39
Faisal-wb <nicebrih@gmail.com>
2026-04-24 13:08:52 +0700

    Faisal

--- Informasi_Sekolah/app/Http/Controllers/AuthController.php
+++ Informasi_Sekolah/app/Http/Controllers/AuthController.php
@@ -2,29 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
-        $request->validate([
-            'email'    => 'required|email|unique:users,email',
-            'username' => 'required|unique:users,name',
-            'password' => 'required|min:6',
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users', 
+            'password' => 'required|string|min:8',
         ]);
 
-        $user = User::create([
-            'name'     => $request->username,
-            'email'    => $request->email,
-            'password' => Hash::make($request->password),
+                if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $user = User::create([  // memasukan imputan ke tabel user
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password), 
         ]);
 
-        return response()->json([
-            'message' => 'Akun berhasil dibuat!',
-            'user'    => $user,
-        ], 201);
+                $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json(['data' => $user, 'access_token' => $token, 'token_type' => 'Bearer'], 201); 
     }
+
 }
