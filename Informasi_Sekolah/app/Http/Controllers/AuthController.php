3bf14839e5fa77da9a2f20a123750f6abe596173
merlinda25 <ndut90920@gmail.com>
2026-05-08 10:09:28 +0700

    update fitur login

--- Informasi_Sekolah/app/Http/Controllers/AuthController.php
+++ Informasi_Sekolah/app/Http/Controllers/AuthController.php
@@ -24,4 +24,11 @@ class AuthController extends Controller
 
         return back()->with('error', 'Email atau password salah');
     }
+
+    public function register(Request $request)
+    {
+        return response()->json([
+            'message' => 'Register berhasil'
+        ]);
+    }
 }
