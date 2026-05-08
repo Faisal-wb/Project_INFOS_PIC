e7fc5dbdae8467096e73a150815d9d77aee82cf0
Faisal-wb <nicebrih@gmail.com>
2026-05-08 09:11:55 +0700

    Faisal

--- Informasi_Sekolah/bootstrap/app.php
+++ Informasi_Sekolah/bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->validateCsrfTokens(except: [
+            '/api/*',
             '/libur/komentar',
         ]);
     })
